546f8230f126fd6bc204114316b3e2d716fcfb76
le_e <duleu.elie@gmail.com>
2015-05-25 00:47:57 +0200

    suppression code inutile p72

--- p72/main.c
+++ p72/main.c
@@ -10,28 +10,13 @@
  * algo correcte et assez rapide pour peu de nombre mais quand limite_y trop grand => les tableaux sont trop grands => deviznt très lent.
  * */
 
-bool frac_irreduc(int y, int x){
-    int r = y%x;
-    while(r != 0){
-	y = x;
-	x = r;
-	r = y%x;
-    }
-    //printf("pgcd(%d, %d) = %d\n", x, y, x);
-    return (x == 1);
-}
-
 int main(){
-    int y = 2, tab_pos = 0; 	    
     long compt = 1;
-    int compt_aff = 0;
     long phi_y;
     int i = 0;
     liste l;
     remplissageListe(&l);
-    //visuListe(l);
     liste p = l;
-    char c;
 
     for(i=3; i<=LIMITE_Y; i++){
 	if(p->suiv!=NULL && i == p->suiv->nbre){
--- p72/nbre.h
+++ p72/nbre.h
@@ -6,15 +6,7 @@
 #include "donnees.h"
 
 bool isPrime(int nbre);
-int nbreNbrePrime(int limite);
-int phi(int nbre, int tab[], int);
-void remplissageTabPrime(int tab[], int, int);
-bool indiceFirstDiviseur(int nbre, int *indice, int tab[]);
-void aff_tab(double tab[], int taille_tab);
-
-bool nbrePrimeTab(int n, int generateur, int tab[]);
-int phi2(int limite, int mul, int tab_prime[]);
-
 long phiListe(int n, int maxPrime, liste l);
+liste searchDeb(int n, liste l);
 
 #endif
