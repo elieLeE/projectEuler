b2cfce5ec1e2729ecc9371fcdf1ec85c5a30c965
le_e <duleu.elie@gmail.com>
2015-06-17 16:34:53 +0200

    ajout suite des nombres carres

--- p66/main.c
+++ p66/main.c
@@ -13,5 +13,17 @@ int main(){
     printf("%s^3 = %s\n", visuAlpha(a), visuAlpha(alphaCubeSur8(a)));
     printf("norme(%s^3) = %lf\n", visuAlpha(a), fabs(normeAlpha(alphaCubeSur8(a))));
 
+    short n = 4, ajoutN = 5;
+    short i;
+    for(i=2; i<=1000; i++){
+	if(i!=n){
+	    printf("i : %d\n", i);
+	}
+	else{
+	    n = n+ajoutN;
+	    ajoutN += 2;
+	}
+    }
+
     return 0;
 }
