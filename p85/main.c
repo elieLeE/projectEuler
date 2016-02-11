065f8ea7bbbea73d93737cd52a36e1658d2be319
le_e <duleu.elie@gmail.com>
2016-02-12 00:26:54 +0100

    p85 resolu

--- p85/main.c
+++ p85/main.c
@@ -13,10 +13,6 @@ int sum(int n){
 }
 
 void miseAJourMin(S_min *min, S_min new){
-    if(new.diff_abs == 16){
-	printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!--- ICI ---!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-    }
-
     if(min->diff_abs > new.diff_abs){
 	*min = new;
     }
@@ -31,34 +27,33 @@ int area(){
 
     for(i=1; i<=1500; i++){
 	first_calcul = sum(i);
-	j = i-1;
+	j = i;
 	calcul = 1;
 	new.i = i;
+	calcul = first_calcul*sum(j);
+	if(calcul > obj){
+	    break;
+	}
 	while(calcul < obj){
 	    j++;
 	    calcul = first_calcul*sum(j);
 	}
 	new.j = j;
 	new.diff_abs = fabs(calcul - obj);
-	printf("after obj => (%d, %d) => %d, %lf\n", i, j, calcul, new.diff_abs);
-	j--;
 	miseAJourMin(&min, new);
 
+	j--;
 	new.j = j;
 	calcul = first_calcul*sum(j);
 	new.diff_abs = fabs(calcul - obj);
-	printf("before obj => (%d, %d) => %d, %lf\n\n", i, j, calcul, new.diff_abs);
 	miseAJourMin(&min, new);
     }
-    printf("(%d, %d) => diff min : %lf ==> %d, diff avec obj => %lf\n", min.i, min.j, min.diff_abs, sum(min.i)*sum(min.j), fabs(sum(min.i)*sum(min.j)-obj));
-    return new.i * new.j;
+    return min.i * min.j;
 }
 
 
 int main(){
     printf("%d\n", area());
-
-    //printf("%d\n", sum(
 
     return 0;
 }
