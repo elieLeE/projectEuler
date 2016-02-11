c39ec0dbbff41c58ac8c72ca79905c9d22887601
le_e <duleu.elie@gmail.com>
2016-02-11 01:01:19 +0100

    nettoyage dossier p96

---
+++ p96/algo1/donnees.h
@@ -0,0 +1,42 @@
+#ifndef _DONNEES_H_
+#define _DONNEES_H_
+
+#define NBRE_LIGNE 9
+#define NBRE_COL 9
+#define NBRE_VAL_POSS 9
+
+typedef struct liste_elem *liste_elem;
+typedef struct liste_elem_imp *liste_elem_imp;
+typedef struct liste_val_poss *liste_val_poss;
+typedef struct S_elem S_elem;
+typedef struct S_pos S_pos;
+
+struct S_pos{
+    char lig, col;
+};
+
+struct liste_elem_imp{
+    char *val;
+    struct S_pos pos;	//pour debugger
+    struct liste_elem_imp *suiv;
+};
+
+struct liste_val_poss{
+    char val;
+    struct liste_val_poss *suiv;
+};
+
+struct S_elem{
+    char *val;
+    struct S_pos pos;	//pour debugger
+    struct liste_val_poss *pos_val_poss;
+    struct liste_val_poss *l_val_poss;
+    struct liste_elem_imp *l_elem_imp;
+};
+
+struct liste_elem{
+    struct S_elem S_el;
+    struct liste_elem *suiv, *prec;
+};
+
+#endif
