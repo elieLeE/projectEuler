34d0b149558f9f7f51f1f8d8888639507e3bd1c6
LI <duleu.elie@gmail.com>
2025-09-10 03:47:59 +0200

    p01: just some cleaning.

--- p01/main.c
+++ p01/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "../shared/sum.h"
 
 #define LIMITE 999
@@ -7,12 +6,13 @@
 #define NBRE_2 5
 
 int main(){
-    const unsigned long enDouble = NBRE_1 * NBRE_2;
+    const unsigned int enDouble = NBRE_1 * NBRE_2;
+    int sol;
 
-    printf("%d => %ld\n", NBRE_1, NBRE_1*sum1ToN(LIMITE/NBRE_1));
-    printf("%d => %ld\n", NBRE_2, NBRE_2*sum1ToN(LIMITE/NBRE_2));
-    printf("%ld => %ld\n", enDouble, enDouble*sum1ToN(LIMITE/enDouble));
-    printf("%ld\n", (NBRE_1*sum1ToN(LIMITE/NBRE_1) + NBRE_2*sum1ToN(LIMITE/NBRE_2) - enDouble*sum1ToN(LIMITE/(enDouble))));
+    sol = (NBRE_1*sum1ToN(LIMITE/NBRE_1) + NBRE_2*sum1ToN(LIMITE/NBRE_2) -
+           enDouble*sum1ToN(LIMITE/(enDouble)));
+
+    printf("%d\n", sol);
 
     return 0;
 }
