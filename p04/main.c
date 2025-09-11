3d481255faf9c707b64c971ef67c223e7ccd8e93
LI <duleu.elie@gmail.com>
2025-09-11 18:59:27 +0200

    p04: use libC.
    Use method informing user of a number is a is palindrome defined in
    libC.

--- p04/main.c
+++ p04/main.c
@@ -1,37 +1,6 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-unsigned int nbre_to_tab(char *tab, unsigned int n)
-{
-    unsigned int n2;
-    unsigned int i = 0;
-    char d;
-
-    while (n >= 1) {
-        n2 = n / 10;
-        d = n - n2 * 10;
-        tab[i] = d;
-        n = n2;
-        i++;
-    }
-    return i;
-}
-
-bool is_palindrome(unsigned int n)
-{
-    static char tab_digit[6];
-    unsigned int nbre_digits;
-
-    nbre_digits = nbre_to_tab(tab_digit, n);
-
-    for (unsigned int i = 0; i<nbre_digits / 2; i++) {
-        if (tab_digit[i] != tab_digit[nbre_digits - i - 1]) {
-            return false;
-        }
-    }
-
-    return true;
-}
+#include "../libC/src/math/nbre.h"
 
 int main()
 {
@@ -44,7 +13,7 @@ int main()
         while (m < 1000) {
             x = n * m;
 
-            if (is_palindrome(x)) {
+            if (is_nber_a_palindrome(x)) {
                 if (x > max_palindrome) {
                     max_palindrome = x;
                 }
