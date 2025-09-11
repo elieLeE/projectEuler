4a1398ac217ae0f4bcfca2a48e5b2b8bf82e0344
LI <duleu.elie@gmail.com>
2025-09-11 18:29:33 +0200

    p04: fix indent.

--- p04/main.c
+++ p04/main.c
@@ -1,54 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include "../shared/nbre.h"
 
-unsigned int nbreToTab(char *tab, unsigned int n){
-    unsigned int n2, i = 0;
+unsigned int nbre_to_tab(char *tab, unsigned int n)
+{
+    unsigned int n2;
+    unsigned int i = 0;
     char d;
-    while(n>=1){
-	n2 = n/10;
-	d = n - n2*10;
-	tab[i] = d;
-	n = n2;
-	i++;
+
+    while (n >= 1) {
+        n2 = n / 10;
+        d = n - n2 * 10;
+        tab[i] = d;
+        n = n2;
+        i++;
     }
     return i;
 }
 
-bool isPalindrome(unsigned int n){
-    static char tabDigit[6];
-    unsigned int nbreDigits = nbreToTab(tabDigit, n);
-    unsigned int i;
+bool is_palindrome(unsigned int n)
+{
+    static char tab_digit[6];
+    unsigned int nbre_digits;
 
-    for(i=0; i<nbreDigits/2; i++){
-	if(tabDigit[i] != tabDigit[nbreDigits-i-1]){
-	    return false;
-	}
+    nbre_digits = nbre_to_tab(tab_digit, n);
+
+    for (unsigned int i = 0; i<nbre_digits / 2; i++) {
+        if (tab_digit[i] != tab_digit[nbre_digits - i - 1]) {
+            return false;
+        }
     }
 
     return true;
 }
 
-int main(){
-    unsigned int n = 100, m = 101;
-    unsigned int maxPalindrome = 0, x;
+int main()
+{
+    unsigned int n = 101, max_palindrome = 0;
+    unsigned int x, m;
 
-    while(n<1000){
-	m = n+1;
-	while(m<1000){
-	    x = n*m;
-	    if(isPalindrome(x)){
-		if(x > maxPalindrome){
-		    maxPalindrome = x;
-		}
-	    }
-	    m++;
-	}
-	n++;
+    while (n < 1000) {
+        m = n + 1;
+
+        while (m < 1000) {
+            x = n * m;
+
+            if (is_palindrome(x)) {
+                if (x > max_palindrome) {
+                    max_palindrome = x;
+                }
+            }
+            m++;
+        }
+        n++;
     }
 
-    printf("%d\n", maxPalindrome);
+    printf("%d\n", max_palindrome);
     return 0;
 }
 
