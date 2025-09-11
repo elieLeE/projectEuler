78012d7307a9c833795a23d88484d20028e4bc8b
LI <duleu.elie@gmail.com>
2025-09-11 18:23:18 +0200

    p03: use libC instead of shared.

--- p03/main.c
+++ p03/main.c
@@ -1,34 +1,36 @@
+#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
-#include "../shared/nbre.h"
+#include "../libC/src/math/prime.h"
 
 #define NBRE 600851475143
 
-bool primeDiv(unsigned long div){
-    return ((NBRE%div == 0) && (isPrime(NBRE/div)));
+bool prime_div(unsigned long div)
+{
+    return ((NBRE % div == 0) && (is_prime(NBRE / div)));
 }
 
-int main(){
-    unsigned long div = 3, maxDiv = 0, otherDiv;
+int main()
+{
+    unsigned long div = 3, max_div = 0, other_div;
     unsigned long rac = (unsigned long)sqrt(NBRE);
 
-    while(div<=rac){
-	if(NBRE%div==0){
-	    if((maxDiv < div) && isPrime(div)){
-		maxDiv = div;
-	    }
-	    else{
-		otherDiv = NBRE/div;
-		if((maxDiv < otherDiv) && isPrime(otherDiv)){
-		    maxDiv = otherDiv;
-		}
-	    }
-	}
-	div = div + 2;
+    while(div <= rac) {
+        if(NBRE % div==0) {
+            if((max_div < div) && is_prime(div)) {
+                max_div = div;
+            } else {
+                other_div = NBRE / div;
+
+                if((max_div < other_div) && is_prime(other_div)) {
+                    max_div = other_div;
+                }
+            }
+        }
+        div = div + 2;
     }
 
-    printf("%ld\n", maxDiv);
+    printf("%ld\n", max_div);
 
     return 0;
 }
