8f35ef381e2a74c8abeccd422dc9c4093b5b2ec7
LI <duleu.elie@gmail.com>
2025-09-14 01:23:38 +0200

    p41: implement a better algorithm.

--- p41/main.c
+++ p41/main.c
@@ -1,20 +1,64 @@
-#include "nbre.h"
-#include "math.h"
-#include "../shared/nbre.h"
+#include <stdio.h>
 
-#define FIRST_NBRE 987654321
-#define LAST_NBRE 321
+#include "../libC/src/math/prime.h"
+#include "../libC/src/math/nbre.h"
 
-int main(){
-    unsigned long i = 0;
+/* Create recursively all pandigitals numbers from 987654321 to the 12 until
+ * one of them is a prime */
+static int
+search_pandigital_prime_nber_rec(unsigned char digits[10],
+                                 bool digits_done[10],
+                                 int depth, int last_digit)
+{
+    if (depth == last_digit) {
+        unsigned long n = build_number_from_digits(digits, 0, last_digit);
 
-    for(i=FIRST_NBRE; i>=LAST_NBRE; i-=2){
-	if(isPandigital(i)){
-	    if(isPrime(i)){
-		printf("%ld\n", i);
-		return 0;
-	    }
-	}
+        return is_prime(n) ? 0 : -1;
     }
+
+    for (int i = last_digit; i > 0; i--) {
+        if (digits_done[i]) {
+            continue;
+        }
+
+        digits[depth] = i;
+        digits_done[i] = true;
+
+        if (search_pandigital_prime_nber_rec(digits, digits_done,
+                                             depth + 1, last_digit) == 0)
+        {
+            return 0;
+        }
+        digits_done[i] = false;
+    }
+
+    return -1;
+}
+
+static int
+search_pandigital_prime_nber(unsigned char digits[10], int last_digit)
+{
+    bool digits_done[10] = {false};
+
+    for (int i = last_digit + 1; i < 10; i++) {
+        digits_done[i] = true;
+    }
+
+    return
+        search_pandigital_prime_nber_rec(digits, digits_done, 0, last_digit);
+}
+
+int main()
+{
+    for (int i = 9; i > 0; i--) {
+        unsigned char digits[10] = {0};
+
+        if (search_pandigital_prime_nber(digits, i) == 0) {
+            printf("%ld\n", build_number_from_digits(digits, 0, i));
+
+            break;
+        }
+    }
+
     return 0;
-} 
+}
