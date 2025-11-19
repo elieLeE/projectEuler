431090b1268cb7a6a9b8b9de41c5a81924cf6d0b
LI <duleu.elie@gmail.com>
2025-11-20 00:50:33 +0100

    adding a script in order to check all projects.
    When I modify something on libC, it is very annoying to check if a
    project has been broken by the new modification.
    Add a script that will do that for me.
    It is possible with this script to compile, run or clean the projects.
    It is also possible to filter on a specify project.

---
+++ check_projects.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+# {{{ Variables
+
+COLOR_YELLOW="\x1B[33m"
+COLOR_GREEN="\x1B[0;32m"
+COLOR_BLUE="\x1B[34m"
+COLOR_RED="\x1B[31m"
+RESET_COLOR="\x1B[0m"
+
+COMPILATING_ACTION=1
+RUNNING_ACTION=2
+CLEANING_ACTION=3
+
+all_euler_projects=()
+
+action=
+action_str=
+project=
+
+tmp_file="tmp.txt"
+
+cmd=
+
+# }}}
+# {{{ Parsing arguments
+
+display_usage() {
+    printf "usage: ./run_project.sh a ACTION [ -p PROJECT ]\n"
+}
+
+# NOTE: This requires GNU getopt.  On Mac OS X and FreeBSD, you have to install
+# this separately; see below.
+TEMP=$(getopt -o mrcp: \
+    --long make,run,clean,project: \
+    -n 'check_projects' -- "$@")
+
+if [ $? != 0 ] ; then display_usage ; exit 1 ; fi
+
+# Note the quotes around '$TEMP': they are essential!
+eval set -- "$TEMP"
+
+while true; do
+    case "$1" in
+        --make | -m)
+            action_str="Compiling"
+            action=${COMPILATING_ACTION}
+            shift
+            ;;
+        --clean | -c)
+            action_str="Cleaning"
+            action=${CLEANING_ACTION}
+            shift
+            ;;
+        --run | -r)
+            action_str="Running"
+            action=${RUNNING_ACTION}
+            shift
+            ;;
+        --project | -p)
+            project=$2
+            if [ ! -d $OPTARG ]; then
+                printf "${COLOR_RED}$project does not exist\n${RESET_COLOR}"
+                exit
+            fi
+            shift 2
+            ;;
+        --use_valgrind)
+            USE_VALGRIND=1
+            shift
+            ;;
+        --check_result)
+            CHECK_RESULT=1
+            shift
+            ;;
+        *)
+            break;
+            ;;
+    esac
+done
+
+if [ ! ${action} ]; then
+    display_usage
+    exit
+fi
+
+# }}}
+# {{{ Executing the command
+
+get_all_folders() {
+    local file=$1
+
+    if [ ${project} ]; then
+        if [ -f ${project}/${file} ]; then
+            all_euler_projects+=(${project})
+        else
+            printf "${COLOR_RED} file '$1' has not been found in "\
+                "${project}\n${RESET_COLOR}"
+        fi
+    else
+        for folder in $(ls -d p*); do
+            if [ -f ${folder}/${file} ]; then
+                all_euler_projects+=(${folder})
+            fi
+        done
+    fi
+}
+
+run_cmd () {
+    local folder=$1
+    local cmd_res=0
+
+    (cd ${folder}; ${cmd[@]})
+
+    cmd_res=$?
+
+    if [ ${cmd_res} != 0 ]; then
+        return 1
+    fi
+
+    return ${res}
+}
+
+run_cmd_on_projects() {
+    for ((idx = 0; idx < ${#all_euler_projects[@]}; idx++)) do
+        local folder=${all_euler_projects[${idx}]}
+        local res=
+
+        if [ ${idx} -gt 0 ]; then
+            printf "\n"
+        fi
+
+        printf "${COLOR_BLUE}${action_str} ${folder}${RESET_COLOR}\n"
+
+        run_cmd ${folder};
+
+        res=$?
+        if [ ${res} != 0 ]; then
+            printf "${COLOR_RED}FAILED${RESET_COLOR}\n"
+            break
+        fi
+        printf "${COLOR_GREEN}SUCCESS${RESET_COLOR}\n"
+
+    done
+}
+
+compile_projets() {
+    cmd=(make all)
+
+    get_all_folders Makefile
+    run_cmd_on_projects
+}
+
+run_projets() {
+    if [ ${USE_VALGRIND} == 1 ]; then
+        cmd=(valgrind --log-file=${tmp_valgrind_file} ./prog)
+    else
+        cmd=(./prog)
+    fi
+
+    get_all_folders prog
+    run_cmd_on_projects
+}
+
+clean_projects() {
+    cmd=(make clean)
+
+    get_all_folders Makefile
+    run_cmd_on_projects
+
+}
+
+# }}}
+
+if [ ${action} -eq ${COMPILATING_ACTION} ]; then
+    compile_projets
+elif [ ${action} -eq ${RUNNING_ACTION} ]; then
+    run_projets
+elif [ ${action} -eq ${CLEANING_ACTION} ]; then
+    clean_projects
+fi
+
+if [ -f ${tmp_file} ]; then
+    rm ${tmp_file}
+fi
