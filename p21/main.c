34aa012da00be633f074e63711828516414c55dd
LI <duleu.elie@gmail.com>
2025-12-06 04:31:33 +0100

    Update libC.

--- p21/main.c
+++ p21/main.c
@@ -5,28 +5,6 @@
 
 #define LIMIT 10000
 
-/* The fact of allocating and deallocation the vector decreases a little the
- * performance of this code but it is still very fast, so anyway */
-unsigned long
-get_divisors_sum_of_n(unsigned long n, const gv_t(uint64) *primes)
-{
-    unsigned sum;
-    gv_t(primes_factors) primes_factors;
-    gv_t(uint64) divisors;
-
-    gv_init(&primes_factors);
-    gv_init(&divisors);
-
-    get_all_primes_factors_of_n(n, primes, &primes_factors);
-    get_all_divisors_of_n_from_prime_factors(&primes_factors, &divisors);
-    sum = get_all_proper_divisors_sum_from_all_divisors(n, &divisors);
-
-    gv_wipe(&divisors, NULL);
-    gv_wipe(&primes_factors, NULL);
-
-    return sum;
-}
-
 int main()
 {
     unsigned long amicable_numbers_sum = 0;
@@ -39,13 +17,13 @@ int main()
     for (unsigned long i = 2; i < LIMIT; i++) {
         unsigned long divisors_sum1, divisors_sum2;
 
-        divisors_sum1 = get_divisors_sum_of_n(i, &primes);
+        divisors_sum1 = get_proper_divisors_sum_of_n(i, &primes);
 
-        if (divisors_sum1 <= i) {
+        if (divisors_sum1 <= i || divisors_sum1 > LIMIT) {
             continue;
         }
 
-        divisors_sum2 = get_divisors_sum_of_n(divisors_sum1, &primes);
+        divisors_sum2 = get_proper_divisors_sum_of_n(divisors_sum1, &primes);
         if (divisors_sum2 == i) {
             amicable_numbers_sum += i + divisors_sum1;
         }
