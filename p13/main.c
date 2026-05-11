d889c000e8cc1eb104c407f583cbf67a6c1af35f
LI <duleu.elie@gmail.com>
2026-05-11 12:31:57 +0200

    p13: use 'big numbers' library recently added in libC.
    I am adding a new module in libC letting me handle big numbers (larger
    than ULONG_MAX).
    As this problem is about adding 50 - digits numbers between them, this
    library can be used there, instead of doing again the code.

--- p13/main.c
+++ p13/main.c
@@ -1,21 +1,35 @@
 #include <limits.h>
 
 #include "../libC/src/io/io.h"
-#include "../libC/src/mem/mem.h"
 #include "../libC/src/macros.h"
+#include "../libC/src/math/big_numbers.h"
 
-#define NBER_OF_NBERS 100
-#define NBER_OF_PART 5
-#define SIZE_PART 10
+generic_vector_t(bns, big_number_t);
 
-static int read_numbers(unsigned long nbers[NBER_OF_NBERS][NBER_OF_PART])
+static void free_bns_vector(void **_bn)
 {
+    big_number_t *bn = (big_number_t *)_bn;
+    bn_wipe(bn);
+}
+
+static int read_numbers(gv_t(bns) *nbers)
+{
+    const unsigned int nbers_nb = 100;
+    const unsigned int parts_nb = 5;
+    const unsigned int size_part = 10;
+    const unsigned long limit_part = 10000000000;
+
     FILE *f;
     char c;
-    unsigned int number_counter = 0, digit_counter = 0, part_counter = 0;
+    unsigned int number_counter = 1, digit_counter = 0;
+    int part_counter = parts_nb - 1;
     unsigned long tmp = 0;
+    big_number_t *bn;
 
     f = RETHROW_PN(ouv_fichier("numbers.txt", "r"));
+
+    bn = gv_grow1(nbers);
+    bn_init_with_args(bn, parts_nb, limit_part);
 
     while ((c = getc(f)) != EOF) {
         if (c < 48 || c > 57) {
@@ -24,19 +38,23 @@ static int read_numbers(unsigned long nbers[NBER_OF_NBERS][NBER_OF_PART])
         tmp += (c - '0');
 
         digit_counter++;
-        if (digit_counter == SIZE_PART) {
-            nbers[number_counter][part_counter] = tmp;
-            part_counter++;
+        if (digit_counter == size_part) {
+            bn_set_part(bn, tmp, part_counter);
 
-            if (part_counter == NBER_OF_PART) {
+            part_counter--;
+
+            if (part_counter < 0) {
                 number_counter++;
 
                 digit_counter = 0;
-                part_counter = 0;
+                part_counter = parts_nb - 1;
 
-                if (number_counter > NBER_OF_NBERS) {
+                if (number_counter > nbers_nb) {
                     break;
                 }
+
+                bn = gv_grow1(nbers);
+                bn_init_with_args(bn, 5, limit_part);
             }
             digit_counter = 0;
             tmp = 0;
@@ -50,54 +68,41 @@ static int read_numbers(unsigned long nbers[NBER_OF_NBERS][NBER_OF_PART])
    return 0;
 }
 
-static unsigned long
-get_sum_part_numbers(unsigned long nbers[NBER_OF_NBERS][NBER_OF_PART],
-                     int idx_part)
+static void get_sum(const gv_t(bns) *in, big_number_t *out)
 {
-    unsigned long sum = 0;
+    bn_fast_clear(out);
 
-    for (int i = 0; i < NBER_OF_NBERS; i++) {
-        sum += nbers[i][idx_part];
-    }
+    bn_set_limit(out, in->tab[0].limit);
 
-    return sum;
-}
-
-static void get_sum(unsigned long nbers[NBER_OF_NBERS][NBER_OF_PART],
-                    unsigned long out[NBER_OF_PART])
-{
-    unsigned long tmp;
-    int carry = 0;
-
-     for (int i = NBER_OF_PART - 1; i >= 0; i--) {
-        tmp = get_sum_part_numbers(nbers, i) + carry;
-        carry = tmp / 10000000000;
-
-        out[0] = tmp;
+     for (int i = 0; i < in->len; i++) {
+        bn_add_bn(&(in->tab[i]), out, out);
      }
 }
 
 int main(void)
 {
-     unsigned long nbers[NBER_OF_NBERS][NBER_OF_PART];
-     unsigned long sum[NBER_OF_PART];
-     unsigned long answer;
+    gv_t(bns) nbers;
+    big_number_t sum;
+    char *sum_str;
 
-     for (int i = 0; i < NBER_OF_NBERS; i++) {
-         p_clear(nbers[i], NBER_OF_PART);
-     }
+     gv_init(&nbers);
+     bn_init(&sum);
 
-     if (read_numbers(nbers) != 0) {
+     if (read_numbers(&nbers) != 0) {
         printf ("error when reading numbers\n");
         return -1;
      }
 
-     get_sum(nbers, sum);
-     answer = sum[0];
-     while (answer > 10000000000) {
-         answer = answer / 10;
-     }
-     printf("%ld\n", answer);
+     get_sum(&nbers, &sum);
+
+     sum_str = bn_to_str(&sum);
+     sum_str[11] = '\0';
+
+     printf("%s\n", sum_str);
+
+     p_free((void **)&sum_str);
+     bn_wipe(&sum);
+     gv_wipe(&nbers, free_bns_vector);
 
      return 0;
 }
